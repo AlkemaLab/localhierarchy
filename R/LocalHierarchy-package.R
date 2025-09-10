c5df6d46946b14cfb891e155b647999405311795
Leontine Alkema <lalkema@umass.edu>
2025-09-10 08:20:09 -0400

    a start on fixing dependencies

---
+++ R/LocalHierarchy-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom purrr list_flatten
+#' @importFrom purrr map
+#' @importFrom purrr map_int
+#' @importFrom purrr map2_int
+## usethis namespace: end
+NULL
